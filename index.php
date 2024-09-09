12216f8b00996f505a7febbf8a0d229f53a16182
savchuckvadim <savchuckvadim@gmail.com>
2024-09-09 12:40:52 +0300

    fxd front)

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?
 require_once (__DIR__.'/crest.php');
 
-// $result = CRest::call(
-// 		'crm.contact.get',
-// 		['ID' => '42', ]
-// 	);
-
-// echo '<pre>';
-// 	print_r($result);
-// echo '</pre>';
 ?>
 
 <?php
